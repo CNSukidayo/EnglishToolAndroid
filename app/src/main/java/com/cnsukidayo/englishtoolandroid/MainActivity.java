e03d58506f2893c601db43b9bfb67e774ffbc2c3
sukidayo <5162809+cnsukidayo@user.noreply.gitee.com>
2021-02-20 12:06:52 +0800

    first commit

---
+++ app/src/main/java/com/cnsukidayo/englishtoolandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cnsukidayo.englishtoolandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
