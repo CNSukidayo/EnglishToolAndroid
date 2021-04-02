56d98b1c7ea6e6dfaa2c11069f98a3919fd1d7e7
sukidayo <5162809+cnsukidayo@user.noreply.gitee.com>
2021-04-02 21:03:03 +0800

    解决RecyclerView数据错乱问题

---
+++ app/src/main/java/com/cnsukidayo/englishtoolandroid/context/WrapperAdapter.java
@@ -0,0 +1,8 @@
+package com.cnsukidayo.englishtoolandroid.context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface WrapperAdapter {
+ 
+  RecyclerView.Adapter getWrappedAdapter() ;
+}
